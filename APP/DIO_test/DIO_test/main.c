f21db751863c6279f4cccfd63669b2f3c0f11136
Youssef Abdelmajeed <youssefabdelmajeed77@gmail.com>
2022-10-29 06:57:35 +0200

    added test application for DIO and fixed some bugs

---
+++ APP/DIO_test/DIO_test/main.c
@@ -0,0 +1,25 @@
+/**
+ * @author Youssef Abdelmajeed (youssefabdelmajeed77@gmail.com)
+ * @brief DIO test application 
+ * @version 0.1
+ * @date 2022-10-29
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
+#define F_CPU 1000000UL 
+#include <util/delay.h>
+#include "DIO_INT.h"
+
+int main(void)
+{
+	DIO_setPinDirection(PA0,OUTPUT) ;
+    while (1) 
+    {
+		DIO_setPinValue(PA0,TOGGLE) ;
+		_delay_ms(300) ;
+    }
+}
+
