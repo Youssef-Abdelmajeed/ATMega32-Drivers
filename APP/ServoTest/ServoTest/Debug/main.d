6bf1cb52bab709491097a0841e506295dcb92848
Youssef Abdelmajeed <youssefabdelmajeed77@gmail.com>
2023-01-13 08:16:47 +0200

    servo app test : added a sweep action

--- APP/ServoTest/ServoTest/Debug/main.d
+++ APP/ServoTest/ServoTest/Debug/main.d
@@ -1,5 +1,9 @@
 main.d main.o: .././main.c .././LIB/STD_TYPES.h .././MCAL/DIO/DIO_INT.h \
- .././MCAL/TimerOne/TimerOne_INT.h .././HAL/Servo/Servo_INT.h
+ .././MCAL/TimerOne/TimerOne_INT.h .././HAL/Servo/Servo_INT.h \
+ d:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ d:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ d:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ d:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
 
 .././LIB/STD_TYPES.h:
 
@@ -8,3 +12,11 @@ main.d main.o: .././main.c .././LIB/STD_TYPES.h .././MCAL/DIO/DIO_INT.h \
 .././MCAL/TimerOne/TimerOne_INT.h:
 
 .././HAL/Servo/Servo_INT.h:
+
+d:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+d:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+d:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+d:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
--- APP/ServoTest/ServoTest/main.c
+++ APP/ServoTest/ServoTest/main.c
@@ -1,15 +1,30 @@
+#define F_CPU 8000000UL 
 #include "LIB/STD_TYPES.h"
 #include "MCAL/DIO/DIO_INT.h"
 #include "MCAL/TimerOne/TimerOne_INT.h"
 #include "HAL/Servo/Servo_INT.h"
-
+#include <util/delay.h>
+#define DELAY_TIME 50
 int main(void)
 {
 	Servo_Init() ; 
 	DIO_setPinDirection(PA0,OUTPUT) ; 
     while (1) 
     {
-		Servo_Move(PD5,90) ;
+		int i  ; 
+		for(i = -90 ; i<=90 ; i++)
+		{
+			Servo_Move(PD5,i) ;
+			_delay_ms(DELAY_TIME) ; 
+		}
+		_delay_ms(DELAY_TIME+50) ;
+		for(i = 90 ; i>=-90 ; i--)
+		{
+			Servo_Move(PD5,i) ;
+			_delay_ms(DELAY_TIME) ;
+		}
+		_delay_ms(DELAY_TIME+50) ;
+
 
     }
 }
